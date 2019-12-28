2659883a35c4cded3d521e4a7bcc92c1ec36553b
bubbles <ms381103@g.risd.org>
2019-12-28 15:45:23 -0600

    running

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetoline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetoline.java
@@ -12,39 +12,43 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class Drivetoline extends LinearOpMode {
 
     // Declare OpMode members.
-   private DcMotorEx MotorLeftFront;
-   private DcMotorEx MotorRightFront;
-   private DcMotorEx MotorLeftBack;
-   private DcMotorEx MotorRightBack;
-   private DcMotor MotorIntake1;
-   private DcMotor MotorIntake2;
+    private DcMotorEx MotorLeftFront;
+    private DcMotorEx MotorRightFront;
+    private DcMotorEx MotorLeftBack;
+    private DcMotorEx MotorRightBack;
+    private DcMotor MotorIntake1;
+    private DcMotor MotorIntake2;
 
     @Override
     public void runOpMode() {
-       MotorLeftFront = (DcMotorEx)hardwareMap.dcMotor.get("MotorLeftFront");
-        MotorRightFront =(DcMotorEx) hardwareMap.dcMotor.get("MotorRightFront");
-        MotorLeftBack = (DcMotorEx)hardwareMap.dcMotor.get("MotorLeftBack");
-        MotorRightBack =(DcMotorEx) hardwareMap.dcMotor.get("MotorRightBack");
+        MotorLeftFront = (DcMotorEx) hardwareMap.dcMotor.get("MotorLeftFront");
+        MotorRightFront = (DcMotorEx) hardwareMap.dcMotor.get("MotorRightFront");
+        MotorLeftBack = (DcMotorEx) hardwareMap.dcMotor.get("MotorLeftBack");
+        MotorRightBack = (DcMotorEx) hardwareMap.dcMotor.get("MotorRightBack");
         MotorRightFront.setDirection(DcMotor.Direction.REVERSE);
-         MotorRightBack.setDirection(DcMotor.Direction.REVERSE);
+        MotorRightBack.setDirection(DcMotor.Direction.REVERSE);
         MotorIntake1 = hardwareMap.dcMotor.get("MotorIntake1");
         MotorIntake2 = hardwareMap.dcMotor.get("MotorIntake2");
-    
-      MotorLeftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
-      MotorRightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
-      MotorLeftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
-      MotorRightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
-        
-        
-    waitForStart();
-    MoveFoward(3500,0.75);
-    sleep(3000);
-    
-       }
-        public void MoveFoward(int Position, double SpeedIsKey)
-       {
+
+        MotorLeftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+        MotorRightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+        MotorLeftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+        MotorRightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+
+
+        waitForStart();
+        MoveFoward(3500, 0.75);
+        sleep(3000);
+
+    }
+
+    public void MoveFoward(int Position, double SpeedIsKey) {
         MotorLeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         MotorRightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         MotorRightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         MotorLeftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-          while(!(MotorLeftFront.getCurrentPosition() >= Position || !MotorRightFront.getCurrentPosition)
+        //while (!(MotorLeftFront.getCurrentPosition() >= Position || !MotorRightFront.getCurrentPosition)) {
+
+        //}
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -12,18 +12,18 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "Teleop", group = "A")
 public class Teleop extends LinearOpMode {
 
-// Declare OpMode members.
+    // Declare OpMode members.
     //Motors
-   private DcMotor MotorLeftFront;
-   private DcMotor MotorRightFront;
-   private DcMotor MotorLeftBack;
-   private DcMotor MotorRightBack;
-   private DcMotorSimple MotorIntake1;
-   private DcMotorSimple MotorIntake2;
-   //Servos
-   private Servo platformMover1;
-   private Servo platformMover2;
-   
+    private DcMotor MotorLeftFront;
+    private DcMotor MotorRightFront;
+    private DcMotor MotorLeftBack;
+    private DcMotor MotorRightBack;
+    private DcMotorSimple MotorIntake1;
+    private DcMotorSimple MotorIntake2;
+    //Servos
+    private Servo platformMover1;
+    private Servo platformMover2;
+
     //variables
     private double up = 1;
     private double down = 0;
@@ -38,18 +38,20 @@ public class Teleop extends LinearOpMode {
 
         MotorIntake1 = hardwareMap.get(DcMotorSimple.class, "MotorIntake1");
         MotorIntake2 = hardwareMap.get(DcMotorSimple.class, "MotorIntake2");
-        
+
         //Servos
         platformMover1 = hardwareMap.get(Servo.class, "pushyboy1");
         platformMover2 = hardwareMap.get(Servo.class, "pushyboy2");
-      
-        
-    waitForStart();
-    while(opModeIsActive())
-    {
-      //DriveTrain Code
-      //forward and back
-      MotorRightBack.setPower(gamepad1.left_stick_y);
-      MotorLeftBack.setPower(-gamepad1.left_stick_y);
-      MotorLeftFront.setPower(-gamepad1.left_stick_y);
-      MotorRightFront.setPower(gamepad1.left_stick_y);
+
+
+        waitForStart();
+        while (opModeIsActive()) {
+            //DriveTrain Code
+            //forward and back
+            MotorRightBack.setPower(gamepad1.left_stick_y);
+            MotorLeftBack.setPower(-gamepad1.left_stick_y);
+            MotorLeftFront.setPower(-gamepad1.left_stick_y);
+            MotorRightFront.setPower(gamepad1.left_stick_y);
+        }
+    }
+}
